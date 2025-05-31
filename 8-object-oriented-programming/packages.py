fc58f31f0b3745084c266c932c9fb80bee738354
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2025-05-31 22:29:54 +0200

    Add Week 8 Additional Practice

---
+++ 8-object-oriented-programming/packages.py
@@ -0,0 +1,28 @@
+class Package:
+    def __init__(self, number, sender, recipient, weight):
+        self.number = number
+        self.sender = sender
+        self.recipient = recipient
+        self.weight = weight
+
+    def __str__(self):
+        return (
+            f"Package {self.number}: {self.sender} to {self.recipient}, {self.weight}kg"
+        )
+
+    def calculate_cost(self, cost_per_kg):
+        return self.weight * cost_per_kg
+
+
+def main():
+    # packages = ["Package1: Alice to Bob, 10kg", "Package2: Bob to Charlie, 5kg"]
+
+    packages = [
+        Package(number=1, sender="Alice", recipient="Bob", weight=10),
+        Package(number=2, sender="Bob", recipient="Charlie", weight=5),
+    ]
+    for package in packages:
+        print(f"{package} costs ${package.calculate_cost(cost_per_kg=2)}")
+
+
+main()
